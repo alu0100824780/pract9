2030a3579eaf7e81abd88089b61a30639e49215c
Rudolf Cicko <alu0100824780@ull.edu.es>
2015-11-18 13:42:43 +0000

    Arreglo

--- lib/linkedList.rb
+++ lib/linkedList.rb
@@ -7,66 +7,8 @@ require "linkedList/revista"
 require "linkedList/ebook"
 
 
-# Node (:value, :next, :back)
 
 module LinkedList
 
-	class List
-	      attr_accessor :First, :Last   # First = Tail || Last = Head
-	      attr_reader :Size
-	   def initialize
- 	      @Last = @First = nil
-              @Size = 0
-           end
-
-	   def push_back(v)
-              if(@Size == 0)
-		  @Last = @First = Node.new(v,nil,nil)
-              else
-                  @Last.next = Node.new(v,nil,@Last)
-		  @Last = @Last.next
-              end
-              @Size = @Size + 1 
-           end
-
-	   def push_front(v)
-	      if(@Size == 0)
-		   @Last = @First = Node.new(v,nil,nil)
-	      else
-		   @First = Node.new(v,@First, nil)
-	      end
-	      @Size = @Size + 1
-	   end
-
-	   def pop_front
-              if(@Size == 1)
-		 aux = @First
-		 @First = @Last = nil
-                 @Size = 0
-	      else
-		 aux = @First
-		 @First = @First.next
-		 aux
-              end
-	   end
-
-
-           def at(i)
-              aux = @First
-              j = 0
-              while(j < i) do
-		aux = aux.next
-                j = j + 1
-              end
-              aux
-           end
-
-	   def [] (i)
-	      at(i)
-	   end
-
-
-        end
-  # Your code goes here...
 end
 
